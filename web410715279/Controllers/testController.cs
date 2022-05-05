6d099a2011086091453b6723aa7234ca3cb0ea74
胡家瑞 <xxx664xxx@gmail.com>
2022-05-05 19:13:51 +0800

    新增HW1~3

---
+++ web410715279/Controllers/testController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410715279.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HW1()
+        {
+            return View();
+        }
+
+        public ActionResult HW2()
+        {
+            return View();
+        }
+
+        public ActionResult HW3()
+        {
+            return View();
+        }
+    }
+}
