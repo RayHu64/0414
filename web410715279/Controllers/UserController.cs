eec9a2b4cb6672cc47ed3d34830666d2b9898e94
胡家瑞 <xxx664xxx@gmail.com>
2022-05-19 20:32:36 +0800

    新增view model

--- web410715279/Controllers/UserController.cs
+++ web410715279/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web410715279.ViewModels;
 
 namespace web410715279.Controllers
 {
@@ -11,27 +12,16 @@ namespace web410715279.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
         [HttpPost]
-        public ActionResult SignUp(string name, string account, string password)
+        public ActionResult SignUp(SignUpData data)
         {
-            if (name==null || name=="") {
-                ViewBag.NameMessage = "請輸入姓名";
-            }
-            if (account== null || account == "")
-            {
-                ViewBag.NameMessage = "請輸入帳號";
-            }
-            if (password== null || password == "")
-            {
-                ViewBag.NameMessage = "請輸入密碼";
-            }
 
-            if (name != "" && account != "" && password != "") {
-                ViewBag.Message = "註冊成功";
+            if (ModelState.IsValid) {
+                data.Message = "註冊成功";
             }
-            return View();
+            return View(data);
         }
     }
 }
---
+++ web410715279/ViewModels/SignUpData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web410715279.ViewModels
+{
+    public class SignUpData
+    {
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage = "請輸入姓名")]
+        [MinLength(5, ErrorMessage = "請輸入至少5個數字")]
+        [MaxLength(10, ErrorMessage = "請勿超過10個字")]
+        public string Name { get; set; }
+
+
+
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        [EmailAddress(ErrorMessage = "請輸入正確的格式")]
+
+        public string Account { get; set; } 
+
+
+
+
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "請輸入5 ~ 10 個字")]
+        public string Password { get; set; }
+
+        public string Message { get; set; } 
+    }
+}
